5afbe080dbfa82783848ff27a6580ede59ebac34
Beatriz Milz <beatriz.milz@hotmail.com>
2020-09-23 07:58:35 -0300

    update cv

--- docs/export_cv.R
+++ docs/export_cv.R
@@ -1,8 +1,8 @@
 export_cv <- function(){
-  rmarkdown::render("docs/index.Rmd",  output_dir =  "docs/", output_file = "index.html")
-  rmarkdown::render("docs/index.Rmd",  output_dir =  "docs/", output_file = "resume.html")
+  rmarkdown::render("docs/index.Rmd")
+#  rmarkdown::render("docs/index.Rmd",  output_dir =  "docs/", output_file = "resume.html")
   pagedown::chrome_print("docs/index.html")
-  pagedown::chrome_print("docs/resume.html")
+ # pagedown::chrome_print("docs/resume.html")
 }
 
 export_cv()
