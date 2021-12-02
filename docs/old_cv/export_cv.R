5d8a4d2982c5955978cab4f00f4d22dc3cf3bd5b
Beatriz Milz <milz.bea@gmail.com>
2021-12-02 17:48:06 -0300

    update cv

--- docs/old_cv/export_cv.R
+++
@@ -1,8 +0,0 @@
-export_cv <- function(){
-  rmarkdown::render("docs/cv.Rmd", output_dir = "docs/index.html")
-  rmarkdown::render("docs/resume.Rmd")
-  pagedown::chrome_print("docs/index.html")
-  pagedown::chrome_print("docs/resume.html")
-}
-
-export_cv()
