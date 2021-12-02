324f9078365a92ed41733f4ca55b4ed7327ec53b
Beatriz Milz <milz.bea@gmail.com>
2021-12-02 19:06:01 -0300

    update export cv

--- docs/export_cv.R
+++ docs/export_cv.R
@@ -1,3 +1,3 @@
 pagedown::chrome_print("docs/index.html")
-fs::file_copy(path = "docs/index.pdf", new_path = "docs/resume.pdf")
-fs::file_copy(path = "docs/index.html", new_path = "docs/resume.html")
+fs::file_copy(path = "docs/index.pdf", new_path = "docs/resume.pdf", overwrite = TRUE)
+fs::file_copy(path = "docs/index.html", new_path = "docs/resume.html", overwrite = TRUE)
