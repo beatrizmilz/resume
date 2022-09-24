e848e34e1ebe38914ca6c116151099c0fb703260
Beatriz Milz <milz.bea@gmail.com>
2022-09-24 11:52:42 -0300

    minor fixes!

--- docs/export_cv.R
+++ docs/export_cv.R
@@ -1,3 +1,3 @@
 pagedown::chrome_print("docs/index.html")
-fs::file_copy(path = "docs/index.pdf", new_path = "docs/resume.pdf", overwrite = TRUE)
-fs::file_copy(path = "docs/index.html", new_path = "docs/resume.html", overwrite = TRUE)
+pdftools::pdf_subset(input = "docs/index.pdf", pages =  c(1,2))
+fs::file_copy(path = "docs/index_output.pdf", new_path = "docs/index.pdf", overwrite = TRUE)
