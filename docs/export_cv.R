0cc6e91b2a3ce43a9335c2992dd0e6a5a74cdc2f
Beatriz Milz <milz.bea@gmail.com>
2023-04-23 11:52:04 -0300

    atualiza cv

--- docs/export_cv.R
+++ docs/export_cv.R
@@ -1,3 +1,3 @@
 pagedown::chrome_print("docs/index.html")
-pdftools::pdf_subset(input = "docs/index.pdf", pages =  c(1,2))
+pdftools::pdf_subset(input = "docs/index.pdf", pages =  c(2,3))
 fs::file_copy(path = "docs/index_output.pdf", new_path = "docs/index.pdf", overwrite = TRUE)
